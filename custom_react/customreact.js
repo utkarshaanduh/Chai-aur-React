2c9861069ddd4cd2883efd29535b4418f27a150b
utkarshaanduh <utkarshaanduh@gmail.com>
2024-11-09 00:58:15 +0530

    commit

--- custom_react/customreact.js
+++ custom_react/customreact.js
@@ -1,13 +1,22 @@
 function customRender(reactelement, container){
+    
     const domElement = document.createElement(reactelement.type)
     domElement.innerHTML = reactelement.children
     domElement.setAttribute('href', reactelement.props.href)
     domElement.setAttribute('target', reactelement.props.target)
     
     container.appendChild(domElement)
-}
+    
+
+    // const domElement = document.createElement(reactelement.type)
+    // domElement.innerHTML = reactelement.children
+    // for (const prop in reactelement.props) {
+    //     domElement.setAttribute(prop, reactelement.props[prop])
+    // }
+    //     container.appendChild(domElement)
+    }
 const reactelement = {
-    type: "a",
+    type: "a", 
     props: {
         href: "http://google.com",
         target: "_blank"
@@ -15,6 +24,16 @@ const reactelement = {
     children: "Click me to visit google"
 }
 
+// const reactelement = react.createElement(
+//     'a',
+//     {href: 'https://google.com', target: '_blank'},
+//     'click me to visit Google'
+// )
+
 const mainContainer = document.getElementById("root");
 
 customRender(reactelement, mainContainer)
+
+ReactDOM.createRoot(document.getElementsById('root')).render(
+    reactelement
+)
