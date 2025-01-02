f8f2f5de6f8cfc8206a4a32a8a26360cf4dca17a
utkarshaanduh <utkarshaanduh@gmail.com>
2025-01-02 18:47:32 +0530

    ye bhi commit ho gaya

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <h1>Chai aur React | Utkarsh</h1> 
+      <Chai/>
+    </>   
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h1> Inside ViteChai.jsx</h1>
+    );
+}
+
+export default Chai;
