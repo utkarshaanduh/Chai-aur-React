8517cd2b7adc40bb9290fba20f6e0141f91a157b
utkarshaanduh <utkarshaanduh@gmail.com>
2025-01-06 19:24:58 +0530

    Made some functional changes in the project

--- 02 Counter/src/App.jsx
+++ 02 Counter/src/App.jsx
@@ -8,18 +8,31 @@ import './App.css'
 function App() {
   
   let [counter, setCounter] = useState(0)
+  let [warning_message, warn_function] = useState("")
 
   // let counter = 0
 
   const addValue = () => {
     setCounter(counter+1)
+    if(counter >=0){
+      warn_function(" ")
+    }
     console.log("value added", counter)
   }
 
   const subtractValue = () => {
     counter = counter - 1
-    console.log("value subtracted", counter)
-    setCounter(counter)
+    if(counter < 0){
+      counter = counter + 1
+      console.log("Value can't be negative")
+      warn_function("Value can't be negative")
+      return 
+    }
+    else{
+      console.log("value subtracted", counter)
+      setCounter(counter)
+    }
+    
   }
 
   return (
@@ -32,6 +45,9 @@ function App() {
       </h2>
       <button onClick={addValue}> Add value_{counter}</button><br/><br></br>
       <button onClick={subtractValue}> Subtract value_{counter}</button><br/><br></br>
+      <h2>
+        {warning_message}
+      </h2>
     </>
   )
 }
